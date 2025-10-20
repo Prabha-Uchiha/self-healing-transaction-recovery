c9f2d5083351ff11cf943cc12a2aba8db6a9be58
Prabha-Uchiha <prabhavy15@gmail.com>
2025-10-20 20:23:30 +0530

    Initial setup

---
+++ payment-recovery-system/src/main/java/com/selfhealing/paymentrecovey/payment_recovery_system/PaymentRecoverySystemApplication.java
@@ -0,0 +1,13 @@
+package com.selfhealing.paymentrecovey.payment_recovery_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentRecoverySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentRecoverySystemApplication.class, args);
+	}
+
+}
---
+++ payment-recovery-system/src/test/java/com/selfhealing/paymentrecovey/payment_recovery_system/PaymentRecoverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selfhealing.paymentrecovey.payment_recovery_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRecoverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
